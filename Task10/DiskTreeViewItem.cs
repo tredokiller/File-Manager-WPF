cba49b20c3e12319d1ea6cfe9ab0956002be1cbf
foxstudent103020 <vlad.tredo86@gmail.com>
2023-01-09 20:18:10 +0200

    Updated calculation system, fixed some issues

--- Task10/DiskTreeViewItem.cs
+++ Task10/DiskTreeViewItem.cs
@@ -1,23 +1,26 @@
+using System;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace Task10;
 
 public class DiskTreeViewItem : TreeViewItem , INotifyPropertyChanged
 {
+    
+    private readonly bool _isDrive;
+    
+    private string _sizeOfFolder;
+
+    private static string[] _sizeSyms = {"B" , "KB" , "MB" , "GB" , "TB"};
+
+
+
+
     public string DataPath { get; }
     
-    
-    private long _sizeOfFolder;
-
-    private readonly DiskTreeViewItem _upperItem;
-    
-    public long SizeOfFolder
+    public string SizeOfFolder
     {
         set
         {
@@ -29,62 +32,126 @@ public class DiskTreeViewItem : TreeViewItem , INotifyPropertyChanged
             return _sizeOfFolder;
         }
     }
-    
-    public string FileFolderName { get; }
+    public string DataName { get; }
 
 
 
-    public DiskTreeViewItem(string path , string fileName , DiskTreeViewItem upperItem = null)
+    public DiskTreeViewItem(string path)
     {
-        DataPath = path;
-        FileFolderName = fileName;
-        _upperItem = upperItem;
-
-        //Thread calculateSizeThread = new Thread(CalculatePathSize);
-        //calculateSizeThread.Start();
+        DataPath = path ?? throw new ArgumentNullException(nameof(path));
         
-        CalculatePathSize();
-    }
-
-
-
-
-
-    private async void CalculatePathSize()
-    {
-        await Task.Run(() =>
+        DataName = GetDataName(DataPath);
+        
+        if (DataName == String.Empty)
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(DataPath);
-            long dirSize = 0;
-            try
+            DataName = DataPath;
+            _isDrive = true;
+
+        }
+        
+        Thread calculateSizeThread = new Thread(CalculatePathSize);
+        calculateSizeThread.Start();
+        
+    }
+
+
+
+
+
+    private void CalculatePathSize()
+    {
+        if (_isDrive)
+        {
+            CalculateDriveSize();
+        }
+
+        else
+        {
+            FileAttributes attribute = File.GetAttributes(DataPath);
+            
+            if (attribute.HasFlag(FileAttributes.Directory))
             {
-                dirSize = dirInfo.EnumerateFiles("*", SearchOption.TopDirectoryOnly).Sum(file => file.Length);
+                CalculateDirectorySize(new DirectoryInfo(DataPath));
             }
-            catch
+            else
             {
-                // ignored
+                CalculateFileSize();
             }
 
-            AddSizeToDir(dirSize);
-        });
+        }
+        
     }
-    
+
+
+
+
+    private void CalculateDriveSize()
+    {
+        long dirSize = 0;
+        DriveInfo drive = new DriveInfo(DataPath);
+
+        if (drive.IsReady)
+        { 
+            dirSize = drive.TotalSize - drive.AvailableFreeSpace;
+        }
+        
+        AddSizeToDir(dirSize);
+    }
+
+
+
+    private long CalculateDirectorySize(DirectoryInfo d)
+    {
+        long size = 0;
+
+        try
+        {
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += CalculateDirectorySize(di);
+            }
+        }
+        catch
+
+        {
+            // ignored
+        }
+
+        AddSizeToDir(size);
+        return size;   
+    }
+
+    private void CalculateFileSize()
+    {
+        {
+            FileInfo file = new FileInfo(DataPath);
+            AddSizeToDir(file.Length);
+        }
+       
+    }
     
     private void AddSizeToDir(long size)
-    { 
-        SizeOfFolder += size;
-
-        if (_upperItem != null)
+    {
+        double convertedSize = size;
+        int sizeIndex = 0;
+        for (int i = 0; i < _sizeSyms.Length; i++)
         {
-            _upperItem.AddSizeToDir(size);
+            if (convertedSize > 1024)
+            {
+                sizeIndex += 1;
+                convertedSize /= 1024;
+            }
         }
+
+        SizeOfFolder = Math.Round(convertedSize, 2) + _sizeSyms[sizeIndex];
     }
-    
-    
-    
-    
-    
-    
     
     private void RaisePropertyChanged(string propName)
     {
@@ -95,9 +162,27 @@ public class DiskTreeViewItem : TreeViewItem , INotifyPropertyChanged
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    
+    public static string GetDataName(string path)
     {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //Get last part of the backslash
+        if (string.IsNullOrEmpty(path))
+        {
+            return string.Empty;
+        }
+
+        var normalizedPath = path.Replace('/', '\\');
+
+        var lastIndex = normalizedPath.LastIndexOf('\\');
+
+        if (lastIndex <= 0)
+        {
+            return path;
+        }
+
+
+        return path.Substring(lastIndex + 1);
+
     }
     
 }
